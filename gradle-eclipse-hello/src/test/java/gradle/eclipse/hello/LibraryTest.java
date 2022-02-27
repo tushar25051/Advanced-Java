19b3ece8369f13baf63f4a18b35661f65d78b9e0
Tushar <btech25051.19@bitmesra.ac.in>
2022-02-28 00:11:37 +0530

    created

--- gradle-eclipse-hello/src/test/java/gradle/eclipse/hello/LibraryTest.java
+++ gradle-eclipse-hello/src/test/java/gradle/eclipse/hello/LibraryTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
-    @Test void someLibraryMethodReturnsTrue() {
-      //  Library classUnderTest = new Library();
-      //  assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
-    }
+	@Test
+	void someLibraryMethodReturnsTrue() {
+		// Library classUnderTest = new Library();
+		// assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should
+		// return 'true'");
+	}
 }
